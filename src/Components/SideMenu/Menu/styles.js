6e76685d1393e97bb88767a8b2808b12e5dee130
Matheus <lima.eca@gmail.com>
2019-12-01 16:54:19 -0300

    responsive menu

--- src/Components/SideMenu/Menu/styles.js
+++ src/Components/SideMenu/Menu/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import colors from 'common/colors';
 import definitions from 'common/definitions';
 
 export const MenuStyled = styled.div`
@@ -8,5 +9,14 @@ export const MenuStyled = styled.div`
     height: 200px;
     margin: auto;
     margin-top: 90px;
+  }
+  @media (max-width: ${definitions.DESKTOP_MIN_WIDTH - 1}px) {
+    position: absolute;
+    border-top: 1px solid ${colors.borders};
+    width: 100%;
+    bottom: 0px;
+    display: flex;
+    align-item: center;
+    justify-content: space-between;
   }
 `;
--- src/Components/SideMenu/MenuItem/styles.js
+++ src/Components/SideMenu/MenuItem/styles.js
@@ -5,16 +5,16 @@ import definitions from 'common/definitions';
 
 export const MenuItemStyled = styled.div`
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${colors.sideMenuText};
   @media (min-width: ${definitions.DESKTOP_MIN_WIDTH}px) {
-    color: ${colors.sideMenuText};
     font-size: 18px;
     width: 170px;
     height: 25px;
     margin: auto;
     margin-top: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     .icon-container {
       width: 25px;
       height: inherit;
@@ -28,6 +28,16 @@ export const MenuItemStyled = styled.div`
       align-items: center;
       justify-content: left;
       margin-left: 8px;
+    }
+  }
+  @media (max-width: ${definitions.DESKTOP_MIN_WIDTH -1}px) {
+    border: 1px solid ${colors.borders};
+    height: 50px;
+    font-size: 30px;
+    width: 20%;
+    background-color: black;
+    .option-container {
+      display: none;
     }
   }
 `;
