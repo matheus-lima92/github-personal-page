63ca40ece57947cbd5a5e170ee22b5ba485bfa8e
Matheus <lima.eca@gmail.com>
2020-03-26 20:32:31 -0300

    minor enhancements

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -8,17 +8,19 @@ const About = () => (
     <AboutStyled>
 		<Title title="Sobre" />
         <Paragraph firstParagraph={true}>
-            Olá, meu nome é Matheus Lima, sou brasileiro nascido no estado de São Paulo, e trabalho como engenheiro de software.
+            Olá, meu nome é Matheus Gomes, sou brasileiro nascido no estado de São Paulo, e trabalho como engenheiro de software.
         </Paragraph>
         <Paragraph>
-            Sou formado em engenharia de controle e automação pela Universidade Estadual de Campinas (UNICAMP), e ao longo do curso de engenharia
-            tive contato com diversas áreas, já que conceitos de cálculo, física, computação, engenharia elétrica e mecânica eram parte da grade
-            curricular. Mas de tudo que estudei, o que realmente fez meu olho brilhar foi o desenvolvimento de software.
+            Sou formado em engenharia de controle e automação pela Universidade Estadual de Campinas (UNICAMP). Ao longo do curso de engenharia
+            tive contato com diversas áreas: conceitos de cálculo, física, computação, engenharia elétrica e mecânica eram parte da grade
+            curricular. De tudo que estudei, o que realmente fez meu olho brilhar foi o desenvolvimento de software.
         </Paragraph>
         <Paragraph lastParagraph={true}>
             Meu primeiro trabalho remunerado relacionado à programação foi um projeto de iniciação científica que iniciei em 2012, ainda
             cursando a faculdade, e de lá pra cá já fiz estágio, freelas, trabalhei em empresas dentro e fora do Brasil, e hoje atuo como
-            engenheiro de software na <a href="https://www.bairesdev.com/" target="_blank">BairesDev</a>.
+            engenheiro de software na <a href="https://www.bairesdev.com/" target="_blank" rel="noopener noreferrer">BairesDev</a>, que hoje se encontra
+            entre as <a href="https://finance.yahoo.com/news/bairesdev-recognized-silicon-valley-top-150000742.html" target="_blank" rel="noopener noreferrer">top 10 companhias</a> de
+            mais rápido crescimento do Vale do Silicio.
         </Paragraph>
     </AboutStyled>
 );
